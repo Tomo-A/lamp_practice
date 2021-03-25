6d7771f798201811826b454d5cefd1ea76ef23b6
Tomoyuki Abe <tomo.abe.knjh1221@gmail.com>
2021-03-25 17:19:58 +0900

    XSS対策としてviewにh関数を実装

--- www/view/cart_view.php
+++ www/view/cart_view.php
@@ -32,18 +32,18 @@
             <td><?php print(number_format(h($cart['price']))); ?>円</td>
             <td>
               <form method="post" action="cart_change_amount.php">
-                <input type="number" name="amount" value="<?php print($cart['amount']); ?>">
+                <input type="number" name="amount" value="<?php print(h($cart['amount'])); ?>">
                 個
                 <input type="submit" value="変更" class="btn btn-secondary">
-                <input type="hidden" name="cart_id" value="<?php print($cart['cart_id']); ?>">
+                <input type="hidden" name="cart_id" value="<?php print(h($cart['cart_id'])); ?>">
               </form>
             </td>
-            <td><?php print(number_format($cart['price'] * $cart['amount'])); ?>円</td>
+            <td><?php print(number_format(h($cart['price'] * $cart['amount']))); ?>円</td>
             <td>
 
               <form method="post" action="cart_delete_cart.php">
                 <input type="submit" value="削除" class="btn btn-danger delete">
-                <input type="hidden" name="cart_id" value="<?php print($cart['cart_id']); ?>">
+                <input type="hidden" name="cart_id" value="<?php print(h($cart['cart_id'])); ?>">
               </form>
 
             </td>
@@ -51,7 +51,7 @@
           <?php } ?>
         </tbody>
       </table>
-      <p class="text-right">合計金額: <?php print number_format($total_price); ?>円</p>
+      <p class="text-right">合計金額: <?php print number_format(h($total_price)); ?>円</p>
       <form method="post" action="finish.php">
         <input class="btn btn-block btn-primary" type="submit" value="購入する">
       </form>
